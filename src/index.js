056e1cd7b10c40af13229166416feedde4c7bbb9
Matthias <git@myrho.net>
2018-02-28 15:16:30 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const port = 5454
 
 const wss = new WebSocket.Server({port : port})
 
+const client = redis.createClient({host : "redis"})
+
 wss.on('connection', ws => {
   ws.on('message', message => {
     switch (message.type) {
       case 'register':
+        if( !message.uri ) return
+        client.get(message.uri, (err, res) => {
+          if( !ws ) return
+          ws.send(0)
+        })
         ws.send(JSON.stringify({type:'registered'})) 
     }
     console.log('received: %s', message)
